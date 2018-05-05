95038f1cabc50d430b3e98db24d5b9c1ef51a8f1
aleksey leichenko <ol.leichenko@gmail.com>
2018-05-05 17:02:16 +0300

    hacker_1/2task

--- src/hackerrank/algorithms/PalindromeIndex.java
+++ src/hackerrank/algorithms/PalindromeIndex.java
@@ -1,4 +1,30 @@
 package hackerrank.algorithms;
 
+import java.util.Scanner;
+
 public class PalindromeIndex {
+
+    static int palindromeIndex(String s){
+        char[] str = s.toCharArray();
+        int lengthStr = s.length();
+        for (int i = 0; i < lengthStr; i += 2) {
+            if (s.charAt(i) != s.charAt(lengthStr-i-1))
+                if (s.charAt(i) == s.charAt(i+1)){
+                return lengthStr-i-1;
+                }
+                else {
+                return i;
+                }
+        }
+        return -1;
+        }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            String s = in.next();
+            int result = palindromeIndex(s);
+            System.out.println(result);
+        }
+    }
 }
